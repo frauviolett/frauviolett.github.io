fa98e06fb27206474cc394713a39b1c06ec816ce
frauviolett <violettwelt@gmail.com>
2020-10-30 13:30:12 -0300

    Primeiro commit

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Ah vamos lá, você tem um nome, não é?",
+                    minlength: "your name must consist of at least 2 characters"
+                },
+                subject: {
+                    required: "vamos lá, você tem um assunto, não é?",
+                    minlength: "your subject must consist of at least 4 characters"
+                },
+                number: {
+                    required: "Vamos lá, você tem um número de contato, não é?",
+                    minlength: "your Number must consist of at least 5 characters"
+                },
+                email: {
+                    required: "sem email, sem mensagem"
+                },
+                message: {
+                    required: "um ... sim, você tem que escrever algo para enviar este formulário.",
+                    minlength: "Só isso? tem certeza?"
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
